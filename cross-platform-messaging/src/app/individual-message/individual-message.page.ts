25cc4d90c38d061b851bbcae95f303648c3dd712
NC03 <nc03developer@gmail.com>
2020-02-15 23:15:22 -0800

    infinite scroll

--- cross-platform-messaging/src/app/individual-message/individual-message.page.ts
+++ cross-platform-messaging/src/app/individual-message/individual-message.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-individual-message',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndividualMessagePage implements OnInit {
 
+    
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loadData(ev)
+  {
+      console.log(ev);
   }
 
 }
