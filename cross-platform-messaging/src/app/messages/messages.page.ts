543bf8fc6f3791910dde1112167a96375410d67c
NC03 <nc03developer@gmail.com>
2020-02-15 23:03:57 -0800

    Update messages.page.ts

--- cross-platform-messaging/src/app/messages/messages.page.ts
+++ cross-platform-messaging/src/app/messages/messages.page.ts
@@ -6,14 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.page.scss'],
 })
 export class MessagesPage implements OnInit {
+    public objs;
+
 
   constructor() { }
 
   ngOnInit() {
+      this.objs = this.fetchConversations()
   }
   plusBtn(ev)
   {
     alert(ev);
+  }
+  fetchConversations()
+  {
+      let objects = [
+          {title: "Drop Collaboration Team", desc:"", url: "url"},
+          {title: "Akash and John", desc:"", url: "url"},
+          {title: "John Doe", desc:"", url: "url"},
+          {title: "Jane Doe", desc:"", url: "url"}
+      ]
+      return objects;
   }
 
 }
